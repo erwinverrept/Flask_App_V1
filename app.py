2032db03ac7f5ca54231f5cd3df21e59e8b19942
erwinverrept <erwinverrept@gmail.com>
2025-07-02 16:51:06 +0200

    analyse not werking

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from laser.views import laser_bp
 from turntable.views import turntable_bp
 from analyse.views import analyse_bp
@@ -6,11 +6,15 @@ from analyse.views import analyse_bp
 app = Flask(__name__)
 app.register_blueprint(laser_bp)
 app.register_blueprint(turntable_bp)
-app.register_blueprint(analyse_bp)
+app.register_blueprint(analyse_bp, url_prefix='/analyse')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/analyse')
+def analyse_redirect():
+    return redirect(url_for('analyse.index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
